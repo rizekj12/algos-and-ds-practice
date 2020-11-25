3b538af0fc869d8b4514c91703bf3d143268b896
Joshua Rizek <Joshua Rizek>
2020-11-25 16:07:31 -0500

    added tree folder and began working on basic tree problems

---
+++ Algos/Trees/bst.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          // current = null
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          // current = null
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(val){
+      if(this.root === null) return false
+
+    let current = this.root
+    while(current){
+        if(current.val === val){
+            return true
+        }else if(val < current.val){
+            current = current.left
+
+        }else{
+            current = current.right
+        }
+
+    }
+    return false
+  }
+}
+
+let tree = new BST();
+
+console.log();
